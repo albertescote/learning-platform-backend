d0d5861b0263b8e88356764dd3130b09e595ea04
albertescote <albertescote03@gmail.com>
2024-04-29 16:56:10 +0200

    feat: add healthcheck controller to module controllers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { SignatureController } from './api/signature/signature.controller';
 import { SignatureModule } from '../context/signature/signature.module';
 import { MeetingController } from './api/meeting/meeting.controller';
 import { MeetingModule } from '../context/meeting/meeting.module';
+import { HealthcheckController } from './api/healthcheck/healthcheck.controller';
 
 @Module({
   imports: [SignatureModule, MeetingModule],
-  controllers: [SignatureController, MeetingController],
+  controllers: [HealthcheckController, SignatureController, MeetingController],
 })
 export class AppModule {}
